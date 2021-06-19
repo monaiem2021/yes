0fcfc6310575a6c8f4da230c3e7a0535b1991f51
monaiem2021 <86136724+monaiem2021@users.noreply.github.com>
2021-06-19 06:28:23 +0200

    Add files via upload

---
+++ real.js
@@ -0,0 +1,185 @@
+﻿// all functions of jaket1
+var oredrJaket1 = document.getElementById("orderJaket1");
+var eyJaket1 = document.getElementById("eyJaket1");
+var userChoice = document.getElementById("userChoice");
+var userChoiceText = document.getElementById("userChoiceText");
+var wordAboutProduct = document.getElementById("wordAboutProduct");
+var confirmDiv = document.getElementById("confirmDiv");
+var confirmButton = document.getElementById("confirmButton");
+var exampleModalLabel = document.getElementById("exampleModalLabel");
+
+
+
+//start of: when user clicks on order1 of jaket1 there will be another div appear with details of the product
+orderJaket1.onclick = function () {
+    "use strict";
+    userChoice.style.backgroundImage = "url(images/jaket2.jpg)";
+    wordAboutProduct.innerHTML = "Most sold 2021";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLabel.innerHTML = "jaket1";
+}
+
+eyeJaket1.onclick = function () {
+    "use strict";
+    userChoice.style.backgroundImage = "url(images/jaket2.jpg)";
+    wordAboutProduct.innerHTML = "Most sold 2021";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLongTitle.innerHTML = "jaket1";
+  
+}
+
+
+
+
+// end of: when user clicks on order1 of jaket1 there will be another div appear with details of the product
+
+
+
+
+
+
+
+// all functions of jaket2
+var oredrJaket2 = document.getElementById("orderJaket2");
+var eyeJaket2 = document.getElementById("eyeJaket2");
+var userChoice = document.getElementById("userChoice");
+
+
+//start of: when user clicks on order2 of jaket2 there will be another div appear with details of the product
+orderJaket2.onclick = function () {
+    "use strict";
+     userChoice.style.backgroundImage = "url(images/jaket3.jpg)";
+    wordAboutProduct.innerHTML = "Classic jaket 2021";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLabel.innerHTML = "jaket2";   
+}
+
+eyeJaket2.onclick = function () {
+    "use strict";
+    userChoice.style.backgroundImage = "url(images/jaket3.jpg)";
+    wordAboutProduct.innerHTML = "Most sold 2021";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLongTitle.innerHTML = "jaket2";
+}
+
+// end of: when user clicks on order2 of jaket2 there will be another div appear with details of the product
+
+
+
+
+// all functions of jaket3
+var oredrJaket3 = document.getElementById("order3");
+var eyeJaket3 = document.getElementById("eyeJaket3");
+var userChoice = document.getElementById("userChoice");
+
+
+//start of: when user clicks on order3 of jaket2 there will be another div appear with details of the product
+orderJaket3.onclick = function () {
+    "use strict";
+     userChoice.style.backgroundImage = "url(images/jaket4.jpg)";
+    wordAboutProduct.innerHTML = "good choice for 2021";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLabel.innerHTML = "jaket3";   
+}
+
+eyeJaket3.onclick = function () {
+    "use strict";
+    userChoice.style.backgroundImage = "url(images/jaket4.jpg)";
+    wordAboutProduct.innerHTML = "Most sold 2021";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLabel.innerHTML = "jaket3";
+}
+
+// end of: when user clicks on order3 of jaket3 there will be another div appear with details of the product
+
+
+
+
+
+// all functions of jaket4
+var oredrJaket4 = document.getElementById("order4");
+var eyeJaket4 = document.getElementById("eyeJaket4");
+var userChoice = document.getElementById("userChoice");
+var textUserChoice = document.getElementById("textUserChoice");
+
+//start of: when user clicks on order4 of jaket4 there will be another div appear with details of the product
+orderJaket4.onclick = function () {
+    "use strict";
+     userChoice.style.backgroundImage = "url(images/jaket5.jpg)";
+    wordAboutProduct.innerHTML = "classic with good materials";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLabel.innerHTML = "jaket4";   
+}
+
+eyeJaket4.onclick = function () {
+    "use strict";
+     userChoice.style.backgroundImage = "url(images/jaket5.jpg)";
+    wordAboutProduct.innerHTML = "classic with good materials";
+    wordAboutProduct.style.marginTop = "10px";
+    exampleModalLabel.innerHTML = "jaket4";   
+}
+// end of: when user clicks on order1 of jaket3 there will be another div appear with details of the product
+//
+
+
+
+
+//start of clicking on confirm button to get user information before buying
+
+var info = document.getElementById("info");
+confirmButton.onclick = function (){
+
+    info.style.transform="scale(1)";
+    info.style.zIndex ="2";
+}
+
+
+
+
+//end of clicking on confirm button to get user information before buying
+
+
+
+
+
+
+
+
+var firstList = document.getElementById("firstList");
+var secondList = document.getElementById("secondList");
+var thirdList = document.getElementById("thirdList");
+var fourthList = document.getElementById("fourthList");
+var fifthList = document.getElementById("fifthList");
+var sixthList = document.getElementById("sixthList");
+var seventhList = document.getElementById("seventhList");
+
+
+firstList.style.backgroundColor = "#150E56";
+firstList.style.marginTop = "100px";
+firstList.style.cursor = "100px";
+
+secondList.style.backgroundColor = "#150E56";
+secondList.style.marginTop = "70px";
+
+
+thirdList.style.backgroundColor = "#150E56";
+thirdList.style.marginTop = "70px";
+
+
+fourthList.style.backgroundColor = "#150E56";
+fourthList.style.marginTop = "70px";
+
+
+fifthList.style.backgroundColor = "#150E56";
+fifthList.style.marginTop = "70px";
+
+
+sixthList.style.backgroundColor = "#150E56";
+sixthList.style.marginTop = "70px";
+
+seventhList.style.backgroundColor = "#150E56";
+seventhList.style.marginTop = "70px";
+
+
+
+//
